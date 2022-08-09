bd6d73a369456f038820f56da7c43f42c2a8a4b2
Wei Yin <44796023+YvanYin@users.noreply.github.com>
2022-08-09 17:52:12 +0800

    Update run.sh

--- Test_Minist/run.sh
+++ Test_Minist/run.sh
@@ -3,16 +3,16 @@
 cd ..
 
 # single scale and single crop, it only forwards once per image.  The short edge of the image will be resized to 1080.
-python tools/test.py --config 720_ss
+python tools/test.py --config confgs/test_720_ss
 
 # single scale and single crop, it only forwards once per image. The short edge of the image will be resized to 1080.
-#python tools/test.py --config 1080_ss
+#python tools/test.py --config confgs/test_1080_ss
 
 
 # single scale and mutiple crops. Each image will be splits to 4 crops, which are fed to the model for multiple forwards. The performance and details 
 # will be much better than previous methods. The short edge of the image will be resized to 720.
-#python tools/test.py --config 720_sm
+#python tools/test.py --config confgs/test_720_sm
 
 # single scale and mutiple crops. Each image will be splits to 4 crops, which are fed to the model for multiple forwards. The performance and details 
 # will be much better than previous methods. The short edge of the image will be resized to 720.
-#python tools/test.py --config 1080_sm
+#python tools/test.py --config confgs/test_1080_sm
